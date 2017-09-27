e0f5b01fadc83eb9f5a3e43e32f2a98f3b51ea40
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-09-27 12:23:00 +0300

    task 1-3

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+const port = 8124;
+const startConnect = 'QA';
+
+const client = new net.Socket();
+
+client.setEncoding('utf8');
+
+
+client.connect(port, function() {
+  console.log('Connected');
+  client.write(startConnect);
+});
+
+
+
+client.on('data', function(data) {
+  console.log(data);
+  client.destroy();
+});
+
+
+
+client.on('close', function() {
+  console.log('Connection closed');
+});
---
+++ server.js
@@ -0,0 +1,51 @@
+const net = require('net');
+const port = 8124;
+let seed = 0;
+const serverOK = 'ACK';
+const serverNO = 'DEC';
+const startConnect = 'QA';
+
+const server = net.createServer((client) => {
+  
+
+  client.setEncoding('utf8');
+
+client.on('data',UserDialog);
+   
+
+    function UserDialog(data, err) 
+    {
+        if (!err)
+         {
+            if (data === startConnect) 
+            {
+                client.id = getUniqId();
+                client.write(serverOK);
+                console.log(client.id + "  connected");
+            }
+        }
+         else 
+         {
+            client.write(serverNO);
+            client.write(err);
+
+        }
+    }
+
+  });
+
+
+
+ 
+
+
+
+server.listen(port, () => {
+  console.log("Server listening on localhost: " + port);
+});
+
+
+function getUniqId() 
+{
+    return seed++;
+}
